8f0cc11a21de89d68ffba0b76f73602f477eadad
ruslanmerdeev <r.merdeev@haulmont.com>
2023-07-27 10:30:29 +0400

    [main] Filled up the LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
-        return new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (Integer current : sourceList) {
+            if (current % 2 == 0) {
+                list.addLast(current);
+            } else {
+                list.addFirst(current);
+            }
+        }
+
+        return list;
     }
 }
