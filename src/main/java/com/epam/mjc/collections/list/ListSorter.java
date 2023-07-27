e404f086be73e0253f3ec003fae31c4a086d4f92
ruslanmerdeev <r.merdeev@haulmont.com>
2023-07-27 10:42:01 +0400

    [main] Filled up the ListSorter.java

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,12 +5,18 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
-        return 0;
+        int aNumber = Integer.parseInt(a);
+        int bNumber = Integer.parseInt(b);
+        if (Math.abs(aNumber) != Math.abs(bNumber)) {
+            return Math.abs(aNumber) - Math.abs(bNumber);
+        }
+        return aNumber - bNumber;
     }
 }
