31a56ef39ef5e09fbc55512d62e29ef52982afdc
ruslanmerdeev <r.merdeev@haulmont.com>
2023-07-27 10:27:16 +0400

    [main] Filled up the ArrayListCreator.java

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 2; i < sourceList.size(); i += 3) {
+            String current = sourceList.get(i);
+            list.add(current);
+            list.add(current);
+        }
+
+        return list;
     }
 }
